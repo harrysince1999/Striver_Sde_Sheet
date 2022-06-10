a25a6bdbbdaf1d74df6483308064174c2f26f32b
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2022-06-10 17:36:29 +0530

    Create Middle_Of_Linked_List.cpp

---
+++ Middle_Of_Linked_List.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+       class Node 
+        { 
+        public:
+            int data;
+            Node *next;
+            Node(int data) 
+            {
+               this->data = data;
+              this->next = NULL;
+            }
+        };
+
+*****************************************************************/
+
+Node *findMiddle(Node *head) {
+    if(head==NULL)    return NULL;
+    if(head->next == NULL)    return head;
+    if(head->next->next==NULL)    return head->next;
+    Node* fast=head->next;
+    Node* slow=head;
+    while(fast)
+    {
+        fast=fast->next;
+        if(fast!=NULL)
+            fast=fast->next;
+        slow = slow->next;
+    }
+    return slow;
+}
