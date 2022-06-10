3dfbbaa2f205aa4bf45c4aaab1a8ea5a0938b1e2
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2022-06-10 21:03:35 +0530

    Create Merge_Two_Sorted_Linked_Lists.cpp

---
+++ Merge_Two_Sorted_Linked_Lists.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+************************************************************/
+
+Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+{
+    if(!first)
+    {
+        return second;
+    }
+    if(!second)
+    {
+        return first;
+    }
+
+    if((first -> data) < (second ->data))
+    {
+        first -> next = sortTwoLists(first -> next, second);
+        return first;
+    }
+    else
+    {
+        second -> next = sortTwoLists(first, second -> next);
+        return second;
+    }
+}
+
