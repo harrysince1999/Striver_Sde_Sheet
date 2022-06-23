9c0fd5159bd671890519b686b2dccff5cb7d8e26
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2022-06-23 15:58:43 +0530

    Update Subsets_II.cpp

--- Subsets_II.cpp
+++ Subsets_II.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+void solve(set < vector < int >> &res,vector<int> arr, vector<int> output,int index)
+{
+    
+    if(index == arr.size())
+    {
+        sort(output.begin(),output.end());
+        res.insert(output);
+        return;
+    }
+    
+    solve(res,arr,output,index+1);
+    
+    output.push_back(arr[index]);
+    solve(res,arr,output,index+1);
+}
+vector<vector<int>> uniqueSubsets(int n, vector<int> &arr)
+{
+    vector<vector<int>> ans;
+    vector<int>output;
+    set < vector < int >> res;
+    solve(res,arr,output,0);
 
+    for (auto it = res.begin(); it != res.end(); it++)
+      ans.push_back( * it);
+  
+    return ans;
+}
