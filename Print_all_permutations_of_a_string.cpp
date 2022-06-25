6f6b1749aa2fac45bbfde3d10cd055810516f7c8
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2022-06-25 15:39:28 +0530

    Create Print_all_permutations_of_a_string.cpp

---
+++ Print_all_permutations_of_a_string.cpp
@@ -0,0 +1,19 @@
+void solve(string s,vector<string>&ans,int index)
+{
+    if(index == s.size())
+    {
+        ans.push_back(s);
+        return;
+    }
+    for(int i=index; i<s.size(); i++)
+    {
+        swap(s[i],s[index]);
+        solve(s,ans,index+1);
+        swap(s[i],s[index]);
+    }
+}
+vector<string> findPermutations(string &s) {
+    vector<string>ans;
+    solve(s,ans,0);
+    return ans;
+}
